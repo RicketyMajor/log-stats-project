de60820c57aebf84f213a83015b3d2091adc46a5
Alonso Vera <alonsoveralarach@gmail.com>
2025-12-07 23:57:53 -0300

    feat: se ha mejorado el archivo generate_data.py con más códigos de estado, más rutas y más métodos, además, ahora simula horas aleatorias dentro del día. Por consecuencia, main.py ha sido modificado para que pueda hacer un ranking de rutas más visitadas, métodos HTTP y horas picos del tráfico (con una barra visual incluida).

--- generate_data.py
+++ generate_data.py
@@ -1,18 +1,24 @@
 import random
 import time
+from datetime import datetime, timedelta
 
 FILE_NAME = "server.log"
 NUM_LINES = 10000
 
 IPS = ["192.168.1.1", "10.0.0.1", "172.16.0.5", "192.168.1.200", "8.8.8.8"]
-
 STATUS_CODES = [
     200,
     200,
     200,
+    201,
+    400,
+    401,
+    403,
+    404,
     404,
     500,
-    301
+    502,
+    503
 ]
 
 PATHS = [
@@ -20,16 +26,24 @@ PATHS = [
     "/about",
     "/contact",
     "/api/login",
-    "/assets/logo.png"
+    "/api/products",
+    "/assets/css/style.css",
+    "/assets/js/app.js",
+    "/admin"
 ]
+METHODS = ["GET", "GET", "GET", "POST", "PUT", "DELETE"]
 
-print(f"Generando {FILE_NAME} con {NUM_LINES} lineas...")
+print(
+    f"Generando {FILE_NAME} con {NUM_LINES} lineas distribuidas en 24 horas...")
 
 with open(FILE_NAME, "w", encoding="utf-8") as f:
+    start_time = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
     for _ in range(NUM_LINES):
         ip = random.choice(IPS)
-        timestamp = time.strftime("%d/%b/%Y:%H:%M:%S +0000")
-        method = random.choice(["GET", "POST", "PUT"])
+        random_seconds = random.randint(0, 24 * 60 * 60)
+        current_time = start_time + timedelta(seconds=random_seconds)
+        timestamp = current_time.strftime("%d/%b/%Y:%H:%M:%S +0000")
+        method = random.choice(METHODS)
         path = random.choice(PATHS)
         status = random.choice(STATUS_CODES)
         size = random.randint(100, 5000)
